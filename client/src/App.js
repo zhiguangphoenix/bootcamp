6d733953d30587e0a68e88c1b535c111306cc646
chenzhiguang <chenzhiguang@bytedance.com>
2019-03-05 10:33:10 +0800

    fe文件夹改名为client

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.scss'
+import TopBar from 'components/TopBar.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <TopBar />
+        bootcamp
+      </div>
+    );
+  }
+}
+
+export default App;
